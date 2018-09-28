5d155e1cb63e919a462e86b98308464f0135f68f
amsly <amsly@loyola.edu>
2018-09-28 15:37:35 -0400

    Coded first story section race type now includes verification process

--- game.py
+++ game.py
@@ -4,6 +4,9 @@
 from colorama import *
 import story
 
+# Define variables
+game = 0
+
 # Introduce title
 print(Fore.BLUE + "Tales From an Evil Journal: A Quest for the Lost Book")
 
@@ -11,9 +14,16 @@ print(Fore.BLUE + "Tales From an Evil Journal: A Quest for the Lost Book")
 programStart = 1
 while programStart == 1:
     race_type = input(Fore.GREEN + "What race would you like to play? (Dragonborn, Elf, or Wolf) ")
+    race_type = race_type.lower()
     player_name = input("Please, tell us your name: ")
-    game = 1
+    if race_type == "dragonborn" or race_type == "elf" or race_type == "wolf":
+        game = 1
+    else:
+        print(Fore.RED + "Invalid race type.")
 
 # Starting the game
     while game == 1:
         story.sectionOne()
+        print("The bartender approaches you while drying a few glasses. 'Ya seem quite interested in that there plaque. You another")
+        print("one of them brash explorers?'")
+        isExperienced = input("(yes or no) ")
--- story.py
+++ story.py
@@ -1,10 +1,8 @@
-def sectionOne()
-    print("     Centuries ago there was a great war. In the desert just outside the city of Sandstone the final showdown occurred. \
-A blue dragonborn named Famoria, and his dragon army, defeated the Arconaloth Xalto. The magic book he wielded, The Book \
-of Vile Darkness, was lost after the battle, buried in a tomb beneath the bones of an ancient dragon. \
-    Well, that's what this plaque says on the wall at least... Every time you visit this tavern, Dimdan's Tavern O' the \
-Fallen, you find yourself gazing at the plaque while you drink the heavily spiked ale. Everyone that's gone searching for \
-the book has never returned, and you wonder as you admire your weapons set and equipment whether or not you should attempt \
-it. \
-    The bartender approaches you while drying a few glasses. 'Ya seem quite interested in that there plaque. You another \
-one of them brash explorers?'")
+def sectionOne():
+    print("   Centuries ago there was a great war. In the desert just outside the city of Sandstone the final showdown occurred.")
+    print("A blue dragonborn named Famoria, and his dragon army, defeated the Arconaloth Xalto. The magic book he wielded, The Book")
+    print("of Vile Darkness, was lost after the battle, buried in a tomb beneath the bones of an ancient dragon.")
+    print("   Well, that's what this plaque says on the wall at least... Every time you visit this tavern, Dimdan's Tavern O' the")
+    print("Fallen, you find yourself gazing at the plaque while you drink the heavily spiked ale. Everyone that's gone searching for")
+    print("the book has never returned, and you wonder as you admire your weapons set and equipment whether or not you should attempt")
+    print("it.")
